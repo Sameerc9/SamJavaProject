83da8849f8c5ccd96cfc9b7352569c76f3a2f4ba
Sameerc9 <71833602+Sameerc9@users.noreply.github.com>
2020-09-24 19:49:28 +0530

    Add files via upload

---
+++ DemoClass.java
@@ -0,0 +1,36 @@
+
+public class DemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i,n=100;
+for(i=1;i<=n;i++)
+{
+	
+if(i%3==0 && i%5==0)
+{
+	System.out.println("FizzBuzz");
+}
+	else
+	{
+		if(i%3==0)
+		{
+			System.out.println("Fizz");
+		}
+		else if(i%5==0)
+		{
+		System.out.println("Buzz");
+	}
+		 else
+		 {
+		 	System.out.println(i);
+		 }
+	}
+
+}
+	
+}
+	}
+	
+
+
